02e20c635b6cd153b8174ad2f9fa3fa51771c1e1
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2020-10-03 14:46:13 +0530

    Add files via upload

---
+++ Data Science Using R/Basic/Basic of R.r
@@ -0,0 +1,30 @@
+# Basic Profile 
+Name = "shivendra jha"
+3 -> degree
+5.11->height
+cgpa <- 9.8
+unity <-1 # arrow sud be from data to variable 
+
+
+#ctc 
+bp = 12000
+hra <-7000
+ta <- 4000
+3000-> pf
+ctc = sum(bp,hra,ta,pf)
+
+
+# loan 
+loan = 6500 
+
+remaining = ctc-loan 
+
+# matrix 
+
+a= c (2,3,4,5) # here c is for concatenation 
+b=c (6,7,8,9)
+data_1 = matrix(c(a,b),nrow=4)
+
+# Create a matrix contaning values like  (4,5,6,7)
+d = matrix(c(4,5,6,7),nrow=2)
+mul = d %*% d
