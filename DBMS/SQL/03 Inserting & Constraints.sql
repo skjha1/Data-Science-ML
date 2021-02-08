0485ca0f894c69d81cc97ffdfae9148073fa0bf3
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-02-09 00:29:08 +0530

    Create 03 Inserting & Constraints.sql

---
+++ DBMS/SQL/03 Inserting & Constraints.sql
@@ -0,0 +1,21 @@
+CREATE table student (
+student_id INT PRIMARY KEY, -- auto_increment * this will automatically increment the primary key 
+name varchar(100) NOT NULL, -- this field cant be null in the table
+major varchar(100) UNIQUE -- it shold be unique *DEFAULT 'undecided'
+
+);
+describe student;
+
+DROP TABLE student;
+
+
+-- now we are going to see how to insert value into table 
+SELECT * FROM student; -- select all from student table 
+INSERT INTO student VALUES(1,'RAM','CSE');
+
+INSERT INTO student VALUES(2,'RAMU','BIO');
+
+INSERT INTO student(student_id,name) VALUES (3,'Shyam');
+-- you cant do duplicate entry of primary key 
+
+insert into student value (4,'Geeta','PHY')
