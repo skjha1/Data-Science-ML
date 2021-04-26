3cd5a9dec3960286578b9030dfe7108a2d5afff7
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-04-26 17:42:03 +0530

    Create 07 Update & Delete.sql

---
+++ DBMS/SQL/07 Update & Delete.sql
@@ -0,0 +1,27 @@
+
+DELETE FROM student;
+
+DELETE FROM student
+WHERE student_id = 4;
+
+DELETE FROM student
+WHERE major = 'Sociology' AND name = 'Kate';
+
+UPDATE student
+SET major = 'Undecided';
+
+UPDATE student
+SET name = 'Johnny'
+WHERE student_id = 4;
+
+UPDATE student
+SET major = 'Biological Sciences'
+WHERE major = 'Biology';
+
+UPDATE student
+SET major = 'Biosociology'
+WHERE major = 'Biology' OR major = 'sociology'
+
+UPDATE student
+SET major = 'Undecided', name = 'Tom'
+WHERE student_id = 4;
