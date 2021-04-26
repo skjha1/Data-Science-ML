f6fd4beba73e0edd1b863ab486a6942887f0ead5
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-04-26 17:32:39 +0530

    Update 02 Createing Table.sql

--- DBMS/SQL/02 Createing Table.sql
+++ DBMS/SQL/02 Createing Table.sql
@@ -1,3 +1,26 @@
+
+INT                           -- Whole Numbers
+DECIMAL(M,N)                  -- Decimal Numbers - Exact Value
+VARCHAR(l)                    -- String of text of length l
+BLOB                          -- Binary Large Object, Stores large data
+DATE                          -- 'YYYY-MM-DD'
+TIMESTAMP                     -- 'YYYY-MM-DD HH:MM:SS' - used for recording events
+
+-- Creating tables
+CREATE TABLE student (
+  student_id INT PRIMARY KEY,
+  name VARCHAR(40),
+  major VARCHAR(40)
+  -- PRIMARY KEY(student_id)
+);
+
+DESCRIBE student;
+DROP TABLE student;
+ALTER TABLE student ADD gpa DECIMAL;
+ALTER TABLE student DROP COLUMN gpa;
+
+
+--------------------------------------------------------------------------------------------------------------------------
 CREATE table student (
 student_id INT PRIMARY KEY,
 name varchar(100),
