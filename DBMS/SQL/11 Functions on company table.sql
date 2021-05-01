842afeb99ef418f7bb37e39392933783e14a8e6c
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2021-05-01 19:53:25 +0530

    Create 11 Functions on company table.sql

---
+++ DBMS/SQL/11 Functions on company table.sql
@@ -0,0 +1,27 @@
+
+-- Find the number of employees
+SELECT COUNT(super_id)
+FROM employee;
+
+-- Find the average of all employee's salaries
+SELECT AVG(salary)
+FROM employee;
+
+-- Find the sum of all employee's salaries
+SELECT SUM(salary)
+FROM employee;
+
+-- Find out how many males and females there are
+SELECT COUNT(sex), sex
+FROM employee
+GROUP BY sex
+
+-- Find the total sales of each salesman
+SELECT SUM(total_sales), emp_id
+FROM works_with
+GROUP BY client_id;
+
+-- Find the total amount of money spent by each client
+SELECT SUM(total_sales), client_id
+FROM works_with
+GROUP BY client_id;
