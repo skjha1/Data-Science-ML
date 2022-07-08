08fd828781a5dccb6dba091151feca09c044bc8f
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2022-07-08 16:52:46 +0530

    Update 10 Simple Loop in oracle.sql

--- DBMS/PL-SQL/10 Simple Loop in oracle.sql
+++ DBMS/PL-SQL/10 Simple Loop in oracle.sql
@@ -23,3 +23,17 @@ begin
         end if;
     end loop;
 end;
+/ 
+-- using EXIT WHEN 
+declare
+    v_counter number :=0;
+    v_result number;
+begin
+    loop
+        v_counter := v_counter + 1; -- every time it is getting updated with 1
+        v_result := 19 * v_counter;
+        dbms_output.put_line('19'|| ' x ' || v_counter|| ' = '|| v_result);
+        -- exit condition
+        EXIT WHEN v_counter >=10;
+    end loop;
+end;
