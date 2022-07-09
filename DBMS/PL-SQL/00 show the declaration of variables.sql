386f97ababa3b497c1758eb09f645d20f7aef0a2
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2022-07-09 22:51:00 +0530

    Update 00 show the declaration of variables.sql

--- DBMS/PL-SQL/00 show the declaration of variables.sql
+++ DBMS/PL-SQL/00 show the declaration of variables.sql
@@ -3,9 +3,9 @@
 declare 
     item_number NUMBER(5);  -- sql data type
     item_name   Varchar2(20); -- sql data type 
-    stock_yn    Booleanl; --PL/sql-only data type
-    item_rate   NUMBER(8,2) -- sql data type 
-    item_description varchar2(40) -- sql data type
+    stock_yn    Boolean; --PL/sql-only data type
+    item_rate   NUMBER(8,2); -- sql data type 
+    item_description varchar2(40); -- sql data type
     
 begin 
     NULL;
