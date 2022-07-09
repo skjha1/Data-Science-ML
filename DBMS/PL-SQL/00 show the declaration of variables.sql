848241992df4dda3877fb5c0948da1e307598955
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2022-07-09 22:49:38 +0530

    Create 00 show the declaration of variables.sql

---
+++ DBMS/PL-SQL/00 show the declaration of variables.sql
@@ -0,0 +1,13 @@
+-- Write pl/sql blocks to show the declaration of varibles;
+
+declare 
+    item_number NUMBER(5);  -- sql data type
+    item_name   Varchar2(20); -- sql data type 
+    stock_yn    Booleanl; --PL/sql-only data type
+    item_rate   NUMBER(8,2) -- sql data type 
+    item_description varchar2(40) -- sql data type
+    
+begin 
+    NULL;
+end;
+/
