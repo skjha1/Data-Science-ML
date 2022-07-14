1029638c2e7da970df504556e986461d366a1d54
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2022-07-14 18:36:29 +0530

    Create 11 control statement example.sql

---
+++ DBMS/PL-SQL/Basic to Adv/11 control statement example.sql
@@ -0,0 +1,36 @@
+------------------------------IF STATEMENTS--------------------------------
+set serveroutput on;
+declare
+v_number number := 30;
+begin
+  if v_number < 10 then
+    dbms_output.put_line('I am smaller than 10');
+  elsif v_number < 20 then
+    dbms_output.put_line('I am smaller than 20');
+  elsif v_number < 30 then
+    dbms_output.put_line('I am smaller than 30');
+  else
+    dbms_output.put_line('I am equal or greater than 30');
+  end if;
+end;
+---------------------------------------------------------------------------
+declare
+v_number number := 5;
+v_name varchar2(30) := 'Adam';
+begin
+  if v_number < 10 or v_name = 'Carol' then
+    dbms_output.put_line('HI');
+    dbms_output.put_line('I am smaller than 10');
+  elsif v_number < 20 then
+    dbms_output.put_line('I am smaller than 20');
+  elsif v_number < 30 then
+    dbms_output.put_line('I am smaller than 30');
+  else
+    if v_number is null then 
+      dbms_output.put_line('The number is null..');
+    else
+      dbms_output.put_line('I am equal or greater than 30');
+    end if;
+  end if;
+end;
+---------------------------------------------------------------------------
