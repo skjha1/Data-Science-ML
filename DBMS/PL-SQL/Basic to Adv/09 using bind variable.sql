e7eea983db97fc524a52ef7cc94f04b826bea855
Shivendra k jha <47923680+skjha1@users.noreply.github.com>
2022-07-14 15:13:21 +0530

    Update 09 using bind variable.sql

--- DBMS/PL-SQL/Basic to Adv/09 using bind variable.sql
+++ DBMS/PL-SQL/Basic to Adv/09 using bind variable.sql
@@ -1,17 +1,22 @@
-SET SERVEROUTPUT ON;
-set AUTOPRINT on;
+--------------------------BIND VARIABLES--------------------------
+set serveroutput on;
+set autoprint on;
 variable var_text varchar2(30);
-variable var_number number;
-
-
+variable var_number NUMBER(20);
+variable var_date DATE;
 declare
-    v_text varchar2(30);
-begin 
-    :var_text := 'Hello PL/SQL';
-    :var_number := 4852;
-    v_text := :var_text;
-    -- dbms_output.put_line(v_text);
-    -- dbms_output.put_line(:var_test);
+v_text varchar2(30);
+begin
+:var_text := 'Hello SQL';
+:var_number := 20;
+v_text := :var_text;
+--dbms_output.put_line(v_text);
+--dbms_output.put_line(:var_text);
 end;
-
 print var_text;
+variable var_sql number;
+begin 
+  :var_sql := 100;
+end;
+select * from employees where employee_id = :var_sql;
+------------------------------------------------------------------
